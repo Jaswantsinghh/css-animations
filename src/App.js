58128329e90efafb6d4a61263bc0812f6aa4e1b5
jaswantsinghh <jaswantsinghjsn@gmail.com>
2022-01-30 11:21:51 +0530

    initial work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="curtain_base">
+        <div className="curtain1"></div>
+        <div className="curtain2"></div>
+      </div>
+      <div className="lamp_base">
+        <div className="lamp_rod"></div>
+        <div className="lamp_cup"></div>
+        <div className="lamp_bulb"></div>
+        <div className="lamp_shade"></div>
+      </div>
+      <div className="base">
+      <div className="square1"></div>
+      <div className="square2"></div>
+    </div>
+    <div class="shadow"></div>
     </div>
   );
 }
